a3d25bbbbdaf0b8eb904bd176cd72e60d5e97161
Sal Ferrarello <salcode@salogic.net>
2014-03-05 15:34:05 -0500

    add admin settings page
    First version of admin settings page to toggle whether or
    not the plugin logs outgoing emails

--- lib/admin-settings.php
+++ lib/admin-settings.php
@@ -4,4 +4,92 @@
  * Value set is
  * @fe_stop_emails_log_email a bool value
  */
-error_log('admin-settings.php include in lib');
+
+// Add a menu for our option page
+add_action('admin_menu', 'fe_stop_emails_add_page');
+function fe_stop_emails_add_page() {
+	add_options_page( 'Stop Emails', 'Stop Emails', 'manage_options', 'fe_stop_emails', 'fe_stop_emails_option_page' );
+
+}
+
+// Draw the option page
+function fe_stop_emails_option_page() {
+	?>
+	<div class="wrap">
+        <div id="icon-themes" class="icon32"></div>
+        <h2><?php _e('Stop Emails:', 'stop-emails'); ?></h2>
+		<form action="options.php" method="post">
+			<?php
+                settings_fields('fe_stop_emails_options');
+                do_settings_sections('fe_stop_emails');
+                submit_button();
+            ?>
+		</form>
+	</div>
+	<?php
+}
+
+// Register and define the settings
+add_action('admin_init', 'fe_stop_emails_admin_init');
+function fe_stop_emails_admin_init(){
+	register_setting(
+		'fe_stop_emails_options',
+		'fe_stop_emails_options',
+		'fe_stop_emails_validate_options'
+	);
+	add_settings_section(
+		'fe_stop_emails_main',
+		'Email Logging',
+		'fe_stop_emails_section_text',
+		'fe_stop_emails'
+	);
+	add_settings_field(
+		'log-email',
+		'Log Emails',
+		'fe_stop_emails_setting_radio_btn',
+		'fe_stop_emails',
+		'fe_stop_emails_main'
+	);
+}
+
+// Draw the section header
+function fe_stop_emails_section_text() {
+	echo '<p>Enter your settings here.</p>';
+}
+
+// Display and fill the form field
+function fe_stop_emails_setting_radio_btn() {
+    $options = get_option( 'fe_stop_emails_options' );
+
+    if( isset( $options['log-email'] ) ) {
+        $log_emails = ( $options['log-email'] ? 1 : 0 );
+    } else {
+        $log_emails = 0;
+    }
+
+    $html = '';
+    $html .=  '<input type="radio" id="fe_stop_emails_log_email_false" name="fe_stop_emails_options[log-email]" value="0"' . checked( 0, $log_emails, false ) . '/>';
+    $html .= '<label for="fe_stop_emails_log_email_false">Disabled (default)</label>';
+
+    $html .= '<input type="radio" id="fe_stop_emails_log_email_true" name="fe_stop_emails_options[log-email]" value="1"' . checked( 1, $log_emails, false ) . '/>';
+    $html .= '<label for="fe_stop_emails_log_email_true">Enabled</label>';
+
+    echo $html;
+}
+
+// Validate user input (we want text only)
+function fe_stop_emails_validate_options( $input ) {
+	// Define the array for the updated options
+	$output = array();
+
+	// Loop through each of the options sanitizing the data
+	foreach( $input as $key => $val ) {
+
+            if( isset ( $input[$key] ) ) {
+                    $output[$key] = ( $input[$key] ? 1 : 0 );
+            } // end if
+
+	} // end foreach
+
+	return $output;
+}
