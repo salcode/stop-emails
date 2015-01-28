238fdcbafb43bb5661e0248684ab9fa7d962e9a7
Sal Ferrarello <sal@launchdm.com>
2015-01-28 16:49:26 -0500

    add tests for should_emails_be_logged_to_php_error_log

--- tests/phpunit/tests/stop-emails.php
+++ tests/phpunit/tests/stop-emails.php
@@ -33,4 +33,42 @@ class Tests_Stop_Emails extends WP_UnitTestCase {
 			'global $phpmailer is not an instance of Fe_Stop_Emails_Fake_PHPMailer'
 		);
 	}
+
+	function test_log_to_error_log_default_to_zero() {
+		$stop_emails = new Fe_Stop_Emails;
+		$this->assertFalse( $stop_emails->should_emails_be_logged_to_php_error_log(),
+			'should_emails_be_logged_to_php_error_log did not default to false'
+		);
+	}
+
+	function test_log_to_error_log_filter() {
+		add_filter( 'fe_stop_emails_log_email', '__return_true' );
+		$stop_emails = new Fe_Stop_Emails;
+		$this->assertTrue( $stop_emails->should_emails_be_logged_to_php_error_log(),
+			'should_emails_be_logged_to_php_error_log did not return true when a __return_true filter was applied'
+		);
+	}
+
+	function test_log_to_error_log_read_option_true() {
+		$option = array(
+			'log-email' => 1,
+		);
+		add_option( 'fe_stop_emails_options', $option );
+		$stop_emails = new Fe_Stop_Emails;
+		$this->assertTrue( $stop_emails->should_emails_be_logged_to_php_error_log(),
+			'should_emails_be_logged_to_php_error_log did not return true when the option was set'
+		);
+	}
+
+	function test_log_to_error_log_read_option_false() {
+		$option = array(
+			'log-email' => 0,
+		);
+		add_option( 'fe_stop_emails_options', $option );
+		$stop_emails = new Fe_Stop_Emails;
+		$this->assertFalse( $stop_emails->should_emails_be_logged_to_php_error_log(),
+			'should_emails_be_logged_to_php_error_log did not return false when the option was set'
+		);
+	}
+
 }
