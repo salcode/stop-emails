2ead923712f28b79854baef4ef140886e21436d5
Sal Ferrarello <sal@launchdm.com>
2015-01-29 22:39:54 -0500

    convert leading whitespace to tabs

--- lib/admin-settings.php
+++ lib/admin-settings.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * Admin Settings Page for Stop Emails Plugin
  * Value set is
  * @fe_stop_emails_options a serialized array
@@ -9,29 +9,29 @@
 add_action('admin_menu', 'fe_stop_emails_add_page');
 function fe_stop_emails_add_page() {
 	add_options_page(
-        __( 'Stop Emails', 'stop-emails' ),
-        __( 'Stop Emails', 'stop-emails' ),
-        'manage_options',
-        'fe_stop_emails',
-        'fe_stop_emails_option_page'
-    );
+		__( 'Stop Emails', 'stop-emails' ),
+		__( 'Stop Emails', 'stop-emails' ),
+		'manage_options',
+		'fe_stop_emails',
+		'fe_stop_emails_option_page'
+	);
 }
 
 // Draw the option page
 function fe_stop_emails_option_page() {
-	?>
+?>
 	<div class="wrap">
-        <div id="icon-themes" class="icon32"></div>
-        <h2><?php _e('Stop Emails:', 'stop-emails'); ?></h2>
+		<div id="icon-themes" class="icon32"></div>
+		<h2><?php _e('Stop Emails:', 'stop-emails'); ?></h2>
 		<form action="options.php" method="post">
-			<?php
-                settings_fields('fe_stop_emails_options');
-                do_settings_sections('fe_stop_emails');
-                submit_button();
-            ?>
+<?php
+	settings_fields('fe_stop_emails_options');
+	do_settings_sections('fe_stop_emails');
+	submit_button();
+?>
 		</form>
 	</div>
-	<?php
+<?php
 }
 
 // Register and define the settings
@@ -59,40 +59,40 @@ function fe_stop_emails_admin_init(){
 
 // Draw the section header
 function fe_stop_emails_section_text() {
-    echo '<p>';
-        _e( 'The Stop Emails plugin has the option to log stopped emails to the PHP error log.', 'stop-emails');
-    echo '</p>';
+	echo '<p>';
+	_e( 'The Stop Emails plugin has the option to log stopped emails to the PHP error log.', 'stop-emails');
+	echo '</p>';
 }
 
 // Display and fill the form field
 function fe_stop_emails_setting_radio_btn() {
-    $options = get_option( 'fe_stop_emails_options' );
+	$options = get_option( 'fe_stop_emails_options' );
 
-    if( isset( $options['log-email'] ) ) {
-        $log_emails = ( $options['log-email'] ? 1 : 0 );
-    } else {
-        $log_emails = 0;
-    }
+	if( isset( $options['log-email'] ) ) {
+		$log_emails = ( $options['log-email'] ? 1 : 0 );
+	} else {
+		$log_emails = 0;
+	}
 
-    $html = '';
-    $html .= '<fieldset>';
+	$html = '';
+	$html .= '<fieldset>';
 
-        $html .= '<p>';
-        $html .= '<label for="fe_stop_emails_log_email_false">';
-            $html .=  '<input type="radio" id="fe_stop_emails_log_email_false" name="fe_stop_emails_options[log-email]" value="0"' . checked( 0, $log_emails, false ) . '/>';
-        $html .= __( 'Disable Logging', 'stop-emails' );
-        $html .= '</label>';
-        $html .= '</p>';
+	$html .= '<p>';
+	$html .= '<label for="fe_stop_emails_log_email_false">';
+	$html .=  '<input type="radio" id="fe_stop_emails_log_email_false" name="fe_stop_emails_options[log-email]" value="0"' . checked( 0, $log_emails, false ) . '/>';
+	$html .= __( 'Disable Logging', 'stop-emails' );
+	$html .= '</label>';
+	$html .= '</p>';
 
-        $html .= '<p>';
-        $html .= '<label for="fe_stop_emails_log_email_true">';
-            $html .= '<input type="radio" id="fe_stop_emails_log_email_true" name="fe_stop_emails_options[log-email]" value="1"' . checked( 1, $log_emails, false ) . '/>';
-        $html .= __( 'Log stopped emails to the PHP Error Log', 'stop-emails' );
-        $html .= '</label>';
-        $html .= '</p>';
+	$html .= '<p>';
+	$html .= '<label for="fe_stop_emails_log_email_true">';
+	$html .= '<input type="radio" id="fe_stop_emails_log_email_true" name="fe_stop_emails_options[log-email]" value="1"' . checked( 1, $log_emails, false ) . '/>';
+	$html .= __( 'Log stopped emails to the PHP Error Log', 'stop-emails' );
+	$html .= '</label>';
+	$html .= '</p>';
 
-    $html .= '</fieldset>';
-    echo $html;
+	$html .= '</fieldset>';
+	echo $html;
 }
 
 // Validate user input (we want text only)
@@ -103,9 +103,9 @@ function fe_stop_emails_validate_options( $input ) {
 	// Loop through each of the options sanitizing the data
 	foreach( $input as $key => $val ) {
 
-            if( isset ( $input[$key] ) ) {
-                    $output[$key] = ( $input[$key] ? 1 : 0 );
-            } // end if
+		if( isset ( $input[$key] ) ) {
+			$output[$key] = ( $input[$key] ? 1 : 0 );
+		} // end if
 
 	} // end foreach
 
